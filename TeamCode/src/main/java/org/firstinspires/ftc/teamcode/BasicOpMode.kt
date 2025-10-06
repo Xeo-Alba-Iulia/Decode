d3b30cc854002a2114c74634929543fb75376214
vlad <95431727+Vlad1-1@users.noreply.github.com>
2025-10-06 14:46:37 +0300

    Pendul controlat fara clasa

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode.kt
@@ -2,16 +2,24 @@ package org.firstinspires.ftc.teamcode
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.Servo
 
 @TeleOp
 class BasicOpMode : OpMode() {
+    private lateinit var pendulServo1: Servo
+    private lateinit var pendulServo2: Servo
+
     override fun init() {
         telemetry.addLine("Hello, FTC!")
         telemetry.update()
+        pendulServo1 = hardwareMap.servo["pendulServo1"]
+        pendulServo2 = hardwareMap.servo["pendulServo2"]
     }
 
     override fun loop() {
         telemetry.addLine("Running...")
         telemetry.update()
+        pendulServo1.position = 0.5
+        pendulServo2.position = 0.5
     }
 }
