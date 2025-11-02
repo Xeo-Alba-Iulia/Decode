88822df58a18c8a7467325206ed15a496c2428f0
vlad <95431727+Vlad1-1@users.noreply.github.com>
2025-11-02 14:20:28 +0200

    refactor: Show constants in dash

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedropathing/Constants.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedropathing/Constants.kt
@@ -1,5 +1,7 @@
+@file:Config
 package org.firstinspires.ftc.teamcode.pedropathing
 
+import com.acmerobotics.dashboard.config.Config
 import com.pedropathing.follower.FollowerConstants
 import com.pedropathing.ftc.drivetrains.MecanumConstants
 import com.pedropathing.ftc.localization.constants.PinpointConstants
@@ -8,17 +10,20 @@ import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver
 import com.qualcomm.robotcore.hardware.DcMotorSimple
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
 
-val followerConstants: FollowerConstants = FollowerConstants()
+@JvmField
+var followerConstants: FollowerConstants = FollowerConstants()
 
-val pathConstraints: PathConstraints = PathConstraints(0.99, 100.0, 1.0, 1.0)
+var pathConstraints: PathConstraints = PathConstraints(0.99, 100.0, 1.0, 1.0)
 
-val pinpointConstants: PinpointConstants = PinpointConstants()
+@JvmField
+var pinpointConstants: PinpointConstants = PinpointConstants()
     .distanceUnit(DistanceUnit.CM)
     .forwardPodY(18.0)
     .strafePodX(0.0)
     .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
 
-val mecanumConstants: MecanumConstants = MecanumConstants()
+@JvmField
+var mecanumConstants: MecanumConstants = MecanumConstants()
     .leftFrontMotorName("frontLeft")
     .rightFrontMotorName("frontRight")
     .leftRearMotorName("backLeft")
