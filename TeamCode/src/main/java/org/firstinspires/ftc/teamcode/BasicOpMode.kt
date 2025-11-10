c3f8fdd6e32eee2ad10e0c8f972948a95186fe64
vlad <95431727+Vlad1-1@users.noreply.github.com>
2025-11-10 17:57:48 +0200

    refactor: Remove sorter from BasicOpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode.kt
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
-import org.firstinspires.ftc.teamcode.sorter.Sorter
 import kotlin.math.cos
 import kotlin.math.sin
 
@@ -15,23 +14,16 @@ class BasicOpMode : OpMode() {
 
     lateinit var drive: Drive
     lateinit var odometry: SensorOdometry
-    lateinit var sorter: Sorter
 
     override fun init() {
         drive = Drive(hardwareMap)
         odometry = SensorOdometry(hardwareMap)
-        sorter = Sorter(hardwareMap)
     }
 
     override fun loop() {
         var axial = -gamepad1.left_stick_y.toDouble()
         var lateral = gamepad1.left_stick_x.toDouble()
         val yaw = gamepad1.right_stick_x.toDouble()
-
-        when {
-            gamepad1.crossWasPressed() -> sorter.intakeBall()
-            gamepad1.circleWasPressed() -> sorter.getBall()
-        }
 
         odometry.update()
         val pose = odometry.pose
