4f8a708c38b3f8d55cdb652a82ebc233ff0ea5da
vlad <95431727+Vlad1-1@users.noreply.github.com>
2026-01-21 15:23:26 +0200

    fix: add bulkCaching code in tickFlow

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/metro/CoroutineProviders.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/metro/CoroutineProviders.kt
@@ -1,12 +1,11 @@
 package org.firstinspires.ftc.teamcode.metro
 
+import com.qualcomm.hardware.lynx.LynxModule
+import com.qualcomm.robotcore.hardware.HardwareMap
 import dev.zacsweers.metro.ContributesTo
 import dev.zacsweers.metro.Provides
 import dev.zacsweers.metro.SingleIn
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.flow
@@ -19,15 +18,19 @@ interface CoroutineProviders {
 
     @Provides
     @SingleIn(OpModeScope::class)
-    fun provideOpModeScope(): CoroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
+    fun provideOpModeScope(): CoroutineScope = CoroutineScope(SupervisorJob())
 
     @Provides
     @SingleIn(OpModeScope::class)
-    fun provideTickFlow(scope: CoroutineScope): SharedFlow<Unit> =
+    fun provideTickFlow(scope: CoroutineScope, map: HardwareMap): SharedFlow<Unit> =
         flow {
+            val hubs = map.getAll(LynxModule::class.java)
+            for (hub in hubs) {
+                hub.bulkCachingMode = LynxModule.BulkCachingMode.MANUAL
+            }
             while (true) {
                 emit(Unit)
-                delay(20L)
+                delay(25L)
             }
-        }.shareIn(scope, SharingStarted.Eagerly, replay = 0)
+        }.shareIn(scope + Dispatchers.IO, SharingStarted.Eagerly, replay = 0)
 }
