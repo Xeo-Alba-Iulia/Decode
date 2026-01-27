019cb5864de5019fe96c631bd9f619b58e93d158
vlad <95431727+Vlad1-1@users.noreply.github.com>
2026-01-27 21:56:53 +0200

    feat: SorterOptimized benchmark

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/sorter/SorterOptimized.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/sorter/SorterOptimized.kt
@@ -1,13 +1,18 @@
 package org.firstinspires.ftc.teamcode.sorter
 
+import com.qualcomm.robotcore.util.RobotLog
+import dev.zacsweers.metro.ContributesBinding
 import dev.zacsweers.metro.Inject
 import org.firstinspires.ftc.teamcode.ArtefactType
+import org.firstinspires.ftc.teamcode.metro.OpModeScope
 import org.firstinspires.ftc.teamcode.sorter.SorterWrapped.Companion.HALF_ROTATION
 import org.firstinspires.ftc.teamcode.sorter.SorterWrapped.Companion.OFFSET
 import java.util.*
 import kotlin.math.abs
+import kotlin.time.measureTime
 
 @Inject
+@ContributesBinding(OpModeScope::class, replaces = [SorterImpl::class, SorterWrapped::class])
 class SorterOptimized(private val sorter: SorterWrapped) : Sorter by sorter {
     val closestPosition = List(3) { artefactIdx ->
         buildMap {
@@ -26,15 +31,21 @@ class SorterOptimized(private val sorter: SorterWrapped) : Sorter by sorter {
         }.toSortedMap() as TreeMap
     }
 
-    override suspend fun prepareShoot(type: ArtefactType?) =
-        sorter.artefacts
-            .indices
-            .filter { type?.equals(sorter.artefacts[it]) ?: (sorter.artefacts[it] != null) }
-            .map { it to closestPosition[it].floorEntry((sorter.position * 1000.0).toInt())!!.value }
-            .minByOrNull { (_, pos) -> abs(sorter.position - pos) }
-            ?.let { (idx, pos) ->
-                sorter.position = pos
-                sorter.artefacts[idx] = null
-                true
-            } ?: false
+    override suspend fun prepareShoot(type: ArtefactType?): Boolean {
+        val result: Boolean
+        val usedTime = measureTime {
+            result = sorter.artefacts
+                .indices
+                .filter { type?.equals(sorter.artefacts[it]) ?: (sorter.artefacts[it] != null) }
+                .map { it to closestPosition[it].floorEntry((sorter.position * 1000.0).toInt())!!.value }
+                .minByOrNull { (_, pos) -> abs(sorter.position - pos) }
+                ?.let { (idx, pos) ->
+                    sorter.position = pos
+                    sorter.artefacts[idx] = null
+                    true
+                } ?: false
+        }
+        RobotLog.dd("SorterOptimized", "prepareShoot took $usedTime")
+        return result
+    }
 }
