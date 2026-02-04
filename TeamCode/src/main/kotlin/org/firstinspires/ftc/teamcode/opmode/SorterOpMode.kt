375c98443ac43c67841dd93836ebf8100ebd43dd
vlad <95431727+Vlad1-1@users.noreply.github.com>
2026-02-04 20:32:10 +0200

    feat: log artefact in sorter

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opmode/SorterOpMode.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opmode/SorterOpMode.kt
@@ -1,12 +1,12 @@
 package org.firstinspires.ftc.teamcode.opmode
 
+import android.util.Log
 import com.acmerobotics.dashboard.FtcDashboard
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.util.RobotLog
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.runBlocking
 import org.firstinspires.ftc.robotcore.external.Telemetry
 import org.firstinspires.ftc.teamcode.ArtefactType
 import org.firstinspires.ftc.teamcode.intake.Intake
@@ -25,7 +25,10 @@ open class SorterOpMode : CoroutineOpMode() {
         observers += sorter
 
         intake.artefactFlow
-            .onEach { sorter.intake(it) }
+            .onEach {
+                Log.d("SorterOpMode", "Detected $it")
+                Log.d("SorterOpMode", "stateFlow is: ${intake.stateFlow}")
+            }
             .launchIn(opModeScope)
 
         intake.stateFlow
@@ -41,35 +44,33 @@ open class SorterOpMode : CoroutineOpMode() {
     }
 
     override fun loop() {
-        runBlocking {
-            when {
-                gamepad1.dpadUpWasPressed() -> sorter.intake(ArtefactType.PURPLE)
-                gamepad1.dpadDownWasPressed() -> sorter.intake(ArtefactType.GREEN)
-            }
-
-            when {
-                gamepad1.leftBumperWasPressed() -> sorter.prepareShoot(ArtefactType.PURPLE)
-                gamepad1.rightBumperWasPressed() -> sorter.prepareShoot(ArtefactType.GREEN)
-                gamepad1.backWasPressed() -> sorter.prepareShoot()
-            }
-
-            if (gamepad1.startWasPressed())
-                sorter.prepareIntake()
-
-            when {
-                sorter.isFull -> intake.isRunning = false
-                gamepad1.crossWasPressed() -> intake.isRunning = !intake.isRunning
-                gamepad1.circleWasPressed() -> intake.isOuttake = true
-                gamepad1.circleWasReleased() -> intake.isOuttake = false
-            }
-
-            when {
-                gamepad1.squareWasPressed() -> sorter.isLifting = true
-                gamepad1.squareWasReleased() -> sorter.isLifting = false
-            }
-
-            sorter.position += (gamepad1.right_trigger - gamepad1.left_trigger).toDouble() * 0.001
+        when {
+            gamepad1.dpadUpWasPressed() -> sorter.intake(ArtefactType.PURPLE)
+            gamepad1.dpadDownWasPressed() -> sorter.intake(ArtefactType.GREEN)
         }
+
+        when {
+            gamepad1.leftBumperWasPressed() -> sorter.prepareShoot(ArtefactType.PURPLE)
+            gamepad1.rightBumperWasPressed() -> sorter.prepareShoot(ArtefactType.GREEN)
+            gamepad1.backWasPressed() -> sorter.prepareShoot()
+        }
+
+        if (gamepad1.startWasPressed())
+            sorter.prepareIntake()
+
+        when {
+            sorter.isFull -> intake.isRunning = false
+            gamepad1.crossWasPressed() -> intake.isRunning = !intake.isRunning
+            gamepad1.circleWasPressed() -> intake.isOuttake = true
+            gamepad1.circleWasReleased() -> intake.isOuttake = false
+        }
+
+        when {
+            gamepad1.squareWasPressed() -> sorter.isLifting = true
+            gamepad1.squareWasReleased() -> sorter.isLifting = false
+        }
+
+        sorter.position += (gamepad1.right_trigger - gamepad1.left_trigger).toDouble() * 0.001
     }
 
     override fun stop() {
