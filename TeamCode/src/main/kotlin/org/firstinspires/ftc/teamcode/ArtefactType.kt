42514fb6759a3e371315c1bcdc0137896022f6b2
vlad <95431727+Vlad1-1@users.noreply.github.com>
2026-02-17 20:46:41 +0200

    feat: add Int.toArtefactList function and update compiler options

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/ArtefactType.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/ArtefactType.kt
@@ -3,3 +3,10 @@ package org.firstinspires.ftc.teamcode
 enum class ArtefactType {
     GREEN, PURPLE
 }
+
+fun Int.toArtefactList(): List<ArtefactType> = when (this) {
+    21 -> listOf(GREEN, PURPLE, PURPLE)
+    22 -> listOf(PURPLE, GREEN, PURPLE)
+    23 -> listOf(PURPLE, PURPLE, GREEN)
+    else -> error("Invalid fiducialId: $this")
+}
