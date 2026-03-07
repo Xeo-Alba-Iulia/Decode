ab24af1eabba84f62631ae20d81c5e8e99913f79
vlad <95431727+Vlad1-1@users.noreply.github.com>
2026-03-07 17:54:12 +0200

    feat: bagă o clasă pentru portul prost de motor de pe expansion

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/metro/ShooterProviders.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/metro/ShooterProviders.kt
@@ -2,13 +2,13 @@ package org.firstinspires.ftc.teamcode.metro
 
 import com.qualcomm.hardware.limelightvision.Limelight3A
 import com.qualcomm.robotcore.hardware.DcMotorEx
-import com.qualcomm.robotcore.hardware.DcMotorSimple
 import com.qualcomm.robotcore.hardware.HardwareMap
 import com.qualcomm.robotcore.hardware.Servo
 import dev.zacsweers.metro.ContributesTo
 import dev.zacsweers.metro.Named
 import dev.zacsweers.metro.Provides
 import org.firstinspires.ftc.teamcode.shooter.Shooter
+import org.firstinspires.ftc.teamcode.shooter.StupidOneDirectionMotor
 
 @ContributesTo(HardwareScope::class)
 interface ShooterProviders {
@@ -29,8 +29,7 @@ interface ShooterProviders {
 
     @Provides
     @Named("shooter")
-    fun provideShooterMotor(map: HardwareMap): DcMotorEx =
-        map.getCast<DcMotorEx>("shooter").apply { direction = DcMotorSimple.Direction.REVERSE }
+    fun provideShooterMotor(map: HardwareMap): DcMotorEx = StupidOneDirectionMotor(map.getCast("shooter"))
 
     @Provides
     fun provideLimelight(map: HardwareMap): Limelight3A = map.getAll(Limelight3A::class.java).single()
